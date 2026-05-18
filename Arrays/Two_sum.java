9da9dc0edd6337cdbb26daeb408586d34dad662c
sindhujareddypendyala <sindhujareddypendyala@gmail.com>
2026-05-18 13:13:15 +0530

    Palindrome_Number

--- Arrays/Two_sum.java
+++ Arrays/Two_sum.java
@@ -1,13 +1,4 @@
-package Arrays;
-/*
-Problem: Two Sum
-Platform: LeetCode
-Difficulty: Easy
-Approach: Brute Force
-Time Complexity: O(n^2)
-Space Complexity: O(1)
-*/
-class Solution {
+class Two_sum {
     public int[] twoSum(int[] nums, int target) {
       for(int i=0; i<=nums.length; i++){
         for(int j=i+1; j<nums.length; j++ ){
